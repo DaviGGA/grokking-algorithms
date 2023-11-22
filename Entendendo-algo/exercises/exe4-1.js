d1a6ec6cdabb77b5515e2bf52dd1d29ac817829c
Davi Gregório <106992602+DaviGGA@users.noreply.github.com>
2023-11-22 17:27:42 -0300

    Add files via upload

---
+++ Entendendo-algo/exercises/exe4-1.js
@@ -0,0 +1,11 @@
+// Escreva uma função soma recursivamente
+
+function sum(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+
+    return arr[0] + sum(arr.slice(1))
+}
+
+console.log(sum([1,2,3,4]))
